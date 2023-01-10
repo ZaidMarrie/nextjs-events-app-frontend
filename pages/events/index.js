23bf972452525c3aff82b1d48b33e88e1416e0d5
Zaid Marrie <zaidmarrie27@gmail.com>
2023-01-10 15:12:43 +0200

    Add events and single event routes and data

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,31 @@
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 
-function EventsPage() {
+function EventsPage({ events }) {
 	return (
-		<Layout>
-			<h1>My Events</h1>
-		</Layout>
+		<>
+			<Layout>
+				<h1>Events</h1>
+
+				{events.length === 0 && <h3>No events to show.</h3>}
+
+				{events.map((evt) => (
+					<EventItem key={evt.id} evt={evt} />
+				))}
+			</Layout>
+		</>
 	);
+}
+
+export async function getStaticProps() {
+	const res = await fetch(`${API_URL}/api/events`);
+	const events = await res.json();
+
+	return {
+		props: { events },
+		revalidate: 1,
+	};
 }
 
 export default EventsPage;
